944fdaa4899d3bc6e538171e8cfeba75a507be61
minghl <a879307253@gmail.com>
2023-07-20 15:22:35 +0200

    09 Basic Template

---
+++ 02-Flask会话技术和Flask模版语言/FlaskPro2_模版语言/App/views.py
@@ -0,0 +1,17 @@
+# views.py: 路由+视图函数
+from flask import Blueprint, render_template
+from .models import *
+
+# 蓝图:先声明，还没有创建app
+blue = Blueprint('user',__name__)
+@blue.route('/')
+def home():
+    pass
+
+    data  = {
+        'name':'ikun',
+        'age':26,
+        'likes':['ball','sing','dance','code']
+    }
+    return render_template('home.html', **data)
+
